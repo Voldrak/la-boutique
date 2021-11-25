5f075d25c065b6b4e6aed274d0d9629fa11083c3
Marco Messina <voldrak94@gmail.com>
2021-11-25 19:01:02 +0100

    feat:aggiunte recensioni

--- js/script.js
+++ js/script.js
@@ -154,3 +154,20 @@ setTimeout (() => {
 //   });
 // }
 
+
+const review = document.querySelector(".review");
+
+
+let reviews = new Array();
+reviews[0] = "Molto facile da usare!";
+reviews[1] = "Davvero un bel negozio, molto conveniente";
+reviews[2] = "Ottimo design!";
+
+let counter = 0;
+function loop() {
+    if (counter > 2) counter = 0;
+    document.querySelector('.review').firstElementChild.innerHTML = reviews[counter];
+    counter++;
+    setTimeout(loop, 2000);
+}
+loop();
